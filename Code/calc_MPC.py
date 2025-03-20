b406c098ec9da40f6c00e8bc871a06d32c70f5ec
Toon <toon.vandegenachte@student.kuleuven.be>
2025-03-20 20:48:41 +0100

    MPC update

---
+++ Code/calc_MPC.py
@@ -0,0 +1,19 @@
+import MPC
+import MPC_fast
+import numpy as np
+from sympy import symbols, exp, Abs, tanh, atan, pi
+
+
+w, t = symbols('w t')
+a = 0.01
+b = 1 / 700000
+offset = 60
+k = 1.0
+# time_dependent = (1 + np.tanh(k * (t - offset))) / 2
+objective = np.exp(-a * w ** 2) #+ time_dependent*b * w ** 2
+
+_, w_sol1, _, _ = MPC.solve_interval(objective, total_points = 1000)
+_, w_sol2, _, _ = MPC_fast.solve_interval(objective, total_points = 1000)
+
+_, w_sol3, _, _ = MPC.solve(objective, total_points = 1000)
+_, w_sol4, _, _ = MPC_fast.solve(objective, total_points = 1000)
